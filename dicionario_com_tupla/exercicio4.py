25603fa37e7a4f5349ec1f172cb06738e0a5bea3
humberto-silvah <humbertonoite@gmail.com>
2025-08-29 19:29:27 +0000

    pratica 29 de ago

---
+++ dicionario_com_tupla/exercicio4.py
@@ -0,0 +1,16 @@
+'''Crie um dicionário onde:
+
+    A chave seja o nome do aluno
+    O valor seja uma tupla com 3 notas
+
+Depois, faça:
+
+    Mostrar todas as notas de um aluno.
+    Calcular a média das notas de cada aluno.'''
+
+media= 0
+caderneta = { 'Rita':(8,9.5,10), } 
+
+def media(a,b,c):
+    midia=sum.caderneta['Rita']
+
---
+++ dicionario_com_tupla/exerciocio2.py
@@ -0,0 +1,8 @@
+'''Crie um dicionário chamado agenda onde cada nome da pessoa é a chave,
+e o telefone e o email ficam dentro de uma tupla como valor.
+Exemplo de saída esperada:'''
+
+agenda = {'maria':  ('9999-9999' , 'maria@email.com', 'mailto:maria@email.com')} 
+
+print(agenda['maria'])
+
