50af8901417e783e62addd577456cdd37c8bd3d4
humberto-silvah <humbertonoite@gmail.com>
2025-09-06 19:30:39 +0000

    Implementa funções de busca linear e bubble sort, além de criar classes para gerenciamento de produtos em estoque e questões de revisão.

---
+++ aula - 04/exercicio1.py
@@ -0,0 +1,15 @@
+#Implemente a busca linear que diga se o elemento existe ou não em uma lista de números.
+
+<<<<<<< HEAD
+
+=======
+def busca_linear (lista, valor):
+    for i in range(len(lista)):
+        if (lista[i] == valor):
+            return i
+    return
+
+numeros=(1,2,34,56,7)
+
+print(busca_linear(numeros, 34))
+>>>>>>> 85c6e083f7273ee64cfba36e79b4dca7cdb2646b
