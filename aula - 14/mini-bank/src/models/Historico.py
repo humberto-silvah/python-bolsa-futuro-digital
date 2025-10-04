be2c875503e0fc4feda0e4bfdace898e3916c90f
Humberto  Silva <humbertonoite@gmail.com>
2025-10-04 19:54:34 +0000

    04 de out 2025

---
+++ aula - 14/mini-bank/src/models/Historico.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacoes(self):
+        return self._transacoes
+
+    
+    def adicionar_transacao(self, transacao): # implementar
+        self.transacoes = transacao
+        return self.transacoes    
