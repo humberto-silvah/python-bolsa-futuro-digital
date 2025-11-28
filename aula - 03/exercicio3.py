21927e83a856c0e51c4d481d4cde5e52fe665273
Humberto  Silva <humbertonoite@gmail.com>
2025-11-28 15:39:11 -0300

    Delete aula - 03 directory

--- aula - 03/exercicio3.py
+++
@@ -1,9 +0,0 @@
-'''Crie um dicionário chamado cidades onde a chave seja uma tupla de (latitude, longitude)
-e o valor seja o nome da cidade.
-Inclua pelo menos 3 cidades brasileiras (ex.: São Paulo, Recife, Campina Grande).
-
-Depois, mostre na tela o nome da cidade a partir de suas coordenadas.'''
-
-cidades = { (-23.5489 ,-46.6388): 'Sao Paulo' , (-8.05428,-34.8813): 'Recife' , (-7.23072,-35.8817): 'Campina Grande'} 
-
-print(cidades[(-7.23072,-35.8817)])
